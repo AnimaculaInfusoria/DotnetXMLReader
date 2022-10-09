80b1e38361b394f2297e30510a6019aec6b2dfb8
Lehotsky <Jakub.Lehotsky@dieboldnixdorf.com>
2022-10-09 16:13:29 +0200

    nothing to say, just enjoy

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml;
+
+namespace ReadXMLFile
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            String FilePath = "fkin_path";
+            XmlTextReader reader = new XmlTextReader (FilePath);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write(reader.Name.ToUpper() + "\n");
+
+                        while (reader.MoveToNextAttribute())
+                            Console.WriteLine(reader.Name + reader.Value);
+                        
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine (reader.Value);
+                        break;
+                    
+                }
+            }
+        }
+    }
+}
